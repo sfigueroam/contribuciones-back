8c9b2875e427964a02fdf2e6cfd7abfd24e055b6
joshe <jose@patagonia-it.com>
2018-12-18 16:21:11 -0300

    WIP: separar acceso público y privado

--- proxy-private.js
+++ proxy-private.js
@@ -3,8 +3,24 @@ const proxyPublic = require('./proxy-public');
 
 module.exports.handler = (event, context, callback) => {
 
-    // Incorporar
-    proxyPublic.handler(event, context,callback);
+
+    let bucket = process.env.bucket;
+    let bucketParse = process.env.bucketParse;
+    let keyParse = process.env.keyParse;
+    let queryParametes = event.queryStringParameters;
+    let path  = queryParametes.path;
+    let idParse = path.split('\/')[1];
+
+    util.getConfigParse(bucketParse, keyParse, idParse,  function(err, data){
+        obtenetToken(bucket, data.clientId)
+    });
+
+    let obtenetToken = function (bucket, clientId){
+        util.getTokenByKey(bucket, clientId, function (err, data) {
+            util.obtenerDatos(data, event, callback)
+        });
+    };
+
 
 };
 
--- proxy-public.js
+++ proxy-public.js
@@ -1,6 +1,5 @@
 'use strict';
 const util = require('./util');
-const https = require('https');
 
 module.exports.handler = (event, context, callback) => {
 
@@ -10,7 +9,7 @@ module.exports.handler = (event, context, callback) => {
     console.log('event.body->', JSON.stringify(event));
     console.log('context->', JSON.stringify(context));
      */
-    let method = event.httpMethod;
+
     let bucket = process.env.bucket;
     let bucketParse = process.env.bucketParse;
     let keyParse = process.env.keyParse;
@@ -20,70 +19,21 @@ module.exports.handler = (event, context, callback) => {
 
 
 
-    util.getConfigParse(bucketParse, keyParse, idParse,  function(err, clientId){
-        obtenetToken(bucket, clientId)
+    util.getConfigParse(bucketParse, keyParse, idParse,  function(err, data){
+        if(data.type === 'public') {
+            obtenetToken(bucket, data.clientId)
+        }else{
+            callback("El endpoint es privado")
+        }
     });
 
     let obtenetToken = function (bucket, clientId){
         util.getTokenByKey(bucket, clientId, function (err, data) {
-            obtenerDatos(data)
+            util.obtenerDatos(data, event, callback)
         });
     };
 
 
 
-    let obtenerDatos = function (token) {
 
-        let response = {
-            statusCode: 200,
-            headers: {
-                'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-                'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
-            },
-            body: null,
-        };
-
-        let responseChunks = [];
-        let options = {
-            hostname: process.env.wsHostname,
-            port: process.env.wsPort,
-            path: path,
-            method: method,
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + token
-            }
-        };
-
-        let req = https.request(options, (res) => {
-            res.on('data', (d) => {
-                responseChunks.push(d);
-                //console.log('responseChunks.join()->', responseChunks.join());
-            });
-        });
-
-        req.on('close', () => {
-            response.body =  responseChunks.join('');
-            callback(null, response);
-        });
-
-        req.on('abort', () => {
-            response.statusCode = 500;
-            callback(response, null);
-        });
-
-        req.on('error', () => {
-            response.statusCode = 503;
-            callback(response, null);
-        });
-
-        if(method === "POST" ) {
-            if (event.body !== undefined && event.body !== null) {
-                req.write(event.body);
-            }else{
-                req.write();
-            }
-        }
-        req.end();
-    }
 };
