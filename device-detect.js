b62653e1e32c382240927650d91e11e0ab6ae0f1
joshe <jose@patagonia-it.com>
2019-03-21 12:26:15 -0300

    ADD: Mejora en el código

--- device-detect.js
+++ device-detect.js
@@ -24,6 +24,7 @@ module.exports.handler = async (event, context) => {
         statusCode: 200,
         body: JSON.stringify({
             device: device,
+            header: event.headers
         }),
     };
 
