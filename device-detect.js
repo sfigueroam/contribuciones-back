662bac31f39a690727f3d6b3707de5b8c5794822
joshe <jose@patagonia-it.com>
2019-03-21 12:02:07 -0300

    ADD: endpoint detecta dispositivo

---
+++ device-detect.js
@@ -0,0 +1,30 @@
+'use strict';
+
+module.exports.handler = async (event, context) => {
+
+    let device = {
+        desktop : false,
+        tablet: false,
+        mobile: false,
+        smartTv: false
+
+    };
+
+    if(event.headers['CloudFront-Is-Desktop-Viewer'] === 'true'){
+        device.desktop = true;
+    } else if(event.headers['CloudFront-Is-Tablet-Viewer'] === 'true'){
+        device.tablet = true;
+    } else if(event.headers['CloudFront-Is-Mobile-Viewer'] === 'true'){
+        device.mobile = true;
+    } else if(event.headers['CloudFront-Is-SmartTV-Viewer'] === 'true'){
+        device.smartTv = true;
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            device: device,
+        }),
+    };
+
+};
