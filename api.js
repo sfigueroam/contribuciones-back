e5b41665e6494b0c77c0039a8d7bae95ee97add7
jmora <jmora@tgr.cl>
2019-09-30 14:05:39 +0000

    se agrega un console log para validar error

--- api.js
+++ api.js
@@ -9,7 +9,7 @@ module.exports.handler = (event, context, callback) => {
     let path = event.path.replace('/api' , '');
     let clientId = process.env.clienteId;
 
-    console.log(path);
+    console.log('path para pillar la tontera: ' + path);
 
     util.getTokenByKey(bucket, clientId, function (err, data) {
         util.obtenerDatos(data, event,path, callback)
